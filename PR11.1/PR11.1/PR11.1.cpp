030a091f298b70d07668492cecd0a8d257eaac69
Voloschuk08 <voloschuk07olia@gmail.com>
2024-11-20 01:07:47 +0200

    Add files via upload

---
+++ PR11.1/PR11.1/PR11.1.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+// Функція, яка перевіряє наявність пари "no" або "on" в рядку
+bool ContainsPair(const string& s) {
+    return (s.find("no") != string::npos || s.find("on") != string::npos);
+}
+
+// Функція, яка замінює кожну пару "no" на "***"
+string ReplaceNo(string& s) {
+    size_t pos = 0;
+    while ((pos = s.find("no", pos)) != string::npos) {
+        s.replace(pos, 2, "***");
+        pos += 3; // перемістити позицію на кінець заміни, щоб уникнути зациклення
+    }
+    return s;
+}
+
+// Основна функція
+int main() {
+    string filename;
+    cout << "Enter the name of the text file: ";
+    cin >> filename;
+
+    ifstream inputFile(filename);
+    if (!inputFile) {
+        cerr << "Error: Unable to open file " << filename << endl;
+        return 1;
+    }
+
+    string line;
+    bool containsPair = false;
+
+    cout << "File content with replacements:" << endl;
+    while (getline(inputFile, line)) {
+        if (ContainsPair(line)) {
+            containsPair = true;
+        }
+        cout << ReplaceNo(line) << endl; // Виводимо замінену строку
+    }
+
+    inputFile.close();
+
+    if (containsPair) {
+        cout << "The file contains 'no' or 'on'." << endl;
+    }
+    else {
+        cout << "The file does not contain 'no' or 'on'." << endl;
+    }
+
+    return 0;
+}
---
+++ PR11.1/UnitTest11.1/UnitTest11.1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR11.1/PR11.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest111
+{
+	TEST_CLASS(UnitTest111)
+	{
+	public:
+		
+        TEST_METHOD(TestReplaceNo)
+        {
+            string testStr1 = "no one knows";
+            string result1 = ReplaceNo(testStr1);
+            Assert::AreEqual(string("*** one k***ws"), result1); // Очікується заміна "no" на "***"
+
+            string testStr2 = "on the notion";
+            string result2 = ReplaceNo(testStr2);
+            Assert::AreEqual(string("on the ***tion"), result2); // Замінено лише "no", "on" залишилося
+
+            string testStr3 = "hello world";
+            string result3 = ReplaceNo(testStr3);
+            Assert::AreEqual(string("hello world"), result3); // Без змін, оскільки немає "no"
+		}
+	};
+}
